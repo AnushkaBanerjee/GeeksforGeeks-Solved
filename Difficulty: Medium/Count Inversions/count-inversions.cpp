f35be653f21dda2a06eb93c50a7ae6a554302113
Anushka Banerjee <109134279+AnushkaBanerjee@users.noreply.github.com>
2024-08-31 18:51:32 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Count Inversions/count-inversions.cpp
@@ -0,0 +1,96 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // arr[]: Input Array
+    // N : Size of the Array arr[]
+    // Function to count inversions in the array.
+    long long int merge(long long arr[], int low, int mid, int high) {
+    vector<long long int> temp; // temporary array
+    int left = low;      // starting index of left half of arr
+    int right = mid + 1;   // starting index of right half of arr
+
+    //Modification 1: cnt variable to count the pairs:
+    long long int cnt = 0;
+
+    //storing elements in the temporary array in a sorted manner//
+
+    while (left <= mid && right <= high) {
+        if (arr[left] <= arr[right]) {
+            temp.push_back(arr[left]);
+            left++;
+        }
+        else {
+            temp.push_back(arr[right]);
+            cnt += (mid - left + 1); //Modification 2
+            right++;
+        }
+    }
+
+    // if elements on the left half are still left //
+
+    while (left <= mid) {
+        temp.push_back(arr[left]);
+        left++;
+    }
+
+    //  if elements on the right half are still left //
+    while (right <= high) {
+        temp.push_back(arr[right]);
+        right++;
+    }
+
+    // transfering all elements from temporary to arr //
+    for (int i = low; i <= high; i++) {
+        arr[i] = temp[i - low];
+    }
+
+    return cnt; // Modification 3
+}
+
+long long int mergeSort(long long arr[], int low, int high) {
+    long long int cnt = 0;
+    if (low >= high) return cnt;
+    int mid = (low + high) / 2 ;
+    cnt += mergeSort(arr, low, mid);  // left half
+    cnt += mergeSort(arr, mid + 1, high); // right half
+    cnt += merge(arr, low, mid, high);  // merging sorted halves
+    return cnt;
+}
+    long long int inversionCount(long long arr[], int n) {
+        // Your Code Here
+        int lo = 0;
+        int hi = n-1;
+        long long int count = mergeSort(arr,lo,hi);
+        return count;
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+
+    long long T;
+    cin >> T;
+
+    while (T--) {
+        int N;
+        cin >> N;
+
+        long long A[N];
+        for (long long i = 0; i < N; i++) {
+            cin >> A[i];
+        }
+        Solution obj;
+        cout << obj.inversionCount(A, N) << endl;
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
