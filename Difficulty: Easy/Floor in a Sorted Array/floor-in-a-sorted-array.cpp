cd8905af8e9cb3c1138c7ec44c454978de97e1f0
Anushka Banerjee <109134279+AnushkaBanerjee@users.noreply.github.com>
2024-09-01 18:49:59 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Floor in a Sorted Array/floor-in-a-sorted-array.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // Function to find floor of x
+    // n: size of vector
+    // x: element whose floor is to find
+    int bs(vector<long long> &v, long long n, long long x){
+        long long lo = 0,hi = n - 1,ans = -1;
+        while(lo <= hi){
+            long long mid = lo + (hi-lo)/2;
+            if(v[mid] <= x) {
+                ans= mid;
+                lo = mid+1;
+            }
+            else hi = mid-1;
+        }
+        return ans;
+    }
+    int findFloor(vector<long long> &v, long long n, long long x) {
+
+        // Your code here
+        int idx = bs(v,n,x);
+        return idx;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+
+    long long t;
+    cin >> t;
+
+    while (t--) {
+        long long n;
+        cin >> n;
+        long long x;
+        cin >> x;
+
+        vector<long long> v;
+
+        for (long long i = 0; i < n; i++) {
+            long long temp;
+            cin >> temp;
+            v.push_back(temp);
+        }
+        Solution obj;
+        cout << obj.findFloor(v, n, x) << endl;
+    }
+
+    return 0;
+}
+// } Driver Code Ends
