cd01d7063e83f6e3ed8ee6fd68f6d13a466b344a
Anushka Banerjee <109134279+AnushkaBanerjee@users.noreply.github.com>
2024-11-02 07:02:14 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Largest Element in Array/largest-element-in-array.cpp
+++ Difficulty: Basic/Largest Element in Array/largest-element-in-array.cpp
@@ -1,39 +1,53 @@
 //{ Driver Code Starts
-//Initial Template for C++
-
 #include <bits/stdc++.h>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
 using namespace std;
 
-// } Driver Code Ends
-//User function Template for C++
 
-class Solution
-{
-public:
-    int largest(vector<int> &arr, int n)
-    {
-        sort(arr.begin(),arr.end());
-        return arr[n-1];
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    int largest(vector<int> &arr) {
+        // code here
+        int n = arr.size();
+        int maxEl = INT_MIN;
+        for(auto it:arr){
+            if(maxEl<it){
+                maxEl = it;
+            }
+        }
+        return maxEl;
     }
 };
 
 
 //{ Driver Code Starts.
-int main()
-{
+
+int main() {
+
     int t;
     cin >> t;
-    while (t--)
-    {
-        int n;
-        cin >> n;
-        vector<int>arr(n);
-        for (int i = 0; i < n; i++)
-        {
-            cin >> arr[i];
+    cin.ignore();
+
+    while (t--) {
+        string input;
+        int num;
+        vector<int> arr;
+
+        getline(cin, input);
+        stringstream s2(input);
+        while (s2 >> num) {
+            arr.push_back(num);
         }
+
         Solution ob;
-        cout << ob.largest(arr, n) << "\n";
+        cout << ob.largest(arr) << endl;
+        cout << "~" << endl;
     }
     return 0;
 }
